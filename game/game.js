612e8644c2c4d15e9e478397acf9eaa765f902f2
krithika R <krithika278@gmail.com>
2026-08-18 20:27:11 +0530

    Add files via upload

---
+++ game/game.js
@@ -0,0 +1,15 @@
+﻿import Phaser from 'phaser'
+import TitleScene   from './scenes/TitleScene.js'
+import BedroomScene from './scenes/BedroomScene.js'
+import RoomScene    from './scenes/RoomScene.js'
+import MusicScene   from './scenes/MusicScene.js'
+import EndingScene  from './scenes/EndingScene.js'
+
+new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#000000',
+    physics: { default: 'arcade', arcade: { debug: false } },
+    scene: [TitleScene, BedroomScene, RoomScene, MusicScene, EndingScene]
+})
